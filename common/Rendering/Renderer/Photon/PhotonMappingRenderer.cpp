1be8a7dcaceb9702680457e1577623488566c40f
mmpozulp <mmpozulp@email.wm.edu>
2015-11-15 17:29:31 -0800

    Finished coding assignment7, output should look like assignment7/photon_map.png

--- common/Rendering/Renderer/Photon/PhotonMappingRenderer.cpp
+++ common/Rendering/Renderer/Photon/PhotonMappingRenderer.cpp
@@ -60,20 +60,138 @@ void PhotonMappingRenderer::GenericPhotonMapGeneration(PhotonKdtree& photonMap, 
     }
 }
 
-void PhotonMappingRenderer::TracePhoton(PhotonKdtree& photonMap, Ray* photonRay, glm::vec3 lightIntensity, std::vector<char>& path, float currentIOR, int remainingBounces)
+void PhotonMappingRenderer::TracePhoton(PhotonKdtree& photonMap, Ray* photonRay, glm::vec3 lightIntensity, 
+        std::vector<char>& path, float currentIOR, int remainingBounces)
 {
-    /*
-     * Assignment 7 TODO: Trace a photon into the scene and make it bounce.
-     *    
-     *    How to insert a 'Photon' struct into the photon map.
-     *        Photon myPhoton;
-     *        ... set photon properties ...
-     *        photonMap.insert(myPhoton);
-     */
+
+    // check the recursive base case
+    if (remainingBounces < 0)
+    {
+        return;
+    }
 
     assert(photonRay);
     IntersectionState state(0, 0);
     state.currentIOR = currentIOR;
+
+    // Trace the photon ray and test for intersections
+    bool didIntersect = storedScene->Trace(photonRay, &state);
+
+    if (!didIntersect)
+    {
+        return;
+    }
+
+    // Get the normal to the intersection
+    glm::vec3 normal = state.ComputeNormal();
+
+    // Move the ray to the intersection point + a little offset
+    glm::vec3 intersectionPoint = state.intersectionRay.GetRayPosition(state.intersectionT);
+    photonRay->SetRayPosition(intersectionPoint + normal * SMALL_EPSILON);
+
+    if (path.size() > 1) 
+    {
+        // This photon did NOT come directly from the light, so
+        // create and store a new Photon in the photon map
+
+        Ray toLightRay(glm::vec3(photonRay->GetPosition()), -photonRay->GetRayDirection(), photonRay->GetMaxT());
+        
+        Photon myPhoton;
+        myPhoton.position = glm::vec3(photonRay->GetPosition());
+        myPhoton.intensity = lightIntensity;
+        myPhoton.toLightRay = toLightRay;
+
+        photonMap.insert(myPhoton);
+    }
+
+    // --------determine whether this photon is scattered or absorbed--------
+
+    const MeshObject* hitMeshObject = state.intersectedPrimitive->GetParentMeshObject();
+    const Material* hitMaterial = hitMeshObject->GetMaterial();
+
+    glm::vec3 diffuseResponse = hitMaterial->GetBaseDiffuseReflection();
+
+    // the probability of reflection is the max of the RGB components of the diffuse response
+
+    float max = diffuseResponse.x;
+    if (diffuseResponse.y > max)
+    {
+        max = diffuseResponse.y;
+    }
+
+    if (diffuseResponse.z > max)
+    {
+        max = diffuseResponse.z;
+    }
+
+    // 0 to RAND_MAX
+    float rand_f = static_cast<float>(rand());
+
+    // 0 to 1
+    rand_f /= RAND_MAX;
+
+    if (rand_f > max)
+    {
+        // photon absorbed
+        return;
+    }
+
+    // ------scatter the photon------
+
+    // sample the hemisphere around the point in order to 
+    // pick a diffuse reflection direction
+
+    // 0 to RAND_MAX
+    float u1 = static_cast<float>(rand());
+    float u2 = static_cast<float>(rand());
+
+    // 0 to 1
+    u1 /= RAND_MAX;
+    u2 /= RAND_MAX;
+
+    float r = sqrt(u1);
+    float theta = 2 * PI * u2;
+
+    float x = r * cos(theta);
+    float y = r * sin(theta);
+    float z = sqrt(1 - u1);
+
+    glm::vec3 newDir = glm::normalize(glm::vec3(x, y, z));
+
+    // ---transform from tangent space to world space---
+
+
+    // pick one of (1, 0, 0) (0, 1, 0) (0, 0, 1) as long as
+    // dot product is not close to 1 (therefore not parallel)
+    // to cross the normal with
+
+    glm::vec3 candidate(1.f, 0.f, 0.f);
+
+    float dot = glm::dot(normal, candidate);
+    if (dot > 0.9)
+    {
+        candidate = glm::vec3(0.f, 1.f, 0.f);
+    }
+
+    glm::vec3 tangent = glm::cross(normal, candidate);
+    glm::vec3 bitangent = glm::cross(normal, tangent);
+
+    // construct 3x3 matrix where columns of the matrix are the
+    // tangent, bitangent, and normal vectors (in that order)
+    glm::mat3 mat(tangent, bitangent, normal);
+
+    // multiply 3x3 matrix by the newDir vector in the previous step
+    // to get the diffuse reflection ray direction in world space
+    glm::vec3 worldDir = mat * newDir;
+
+    photonRay->SetRayDirection(worldDir);
+
+    // append to path vector, decrement remaining bounces
+    path.push_back('L');
+    remainingBounces--;
+
+    // recursive call
+    TracePhoton(photonMap, photonRay, lightIntensity, path, currentIOR, remainingBounces);
 }
 
 glm::vec3 PhotonMappingRenderer::ComputeSampleColor(const struct IntersectionState& intersection, const class Ray& fromCameraRay) const
--- common/Scene/Lights/Point/PointLight.cpp
+++ common/Scene/Lights/Point/PointLight.cpp
@@ -15,7 +15,50 @@ float PointLight::ComputeLightAttenuation(glm::vec3 origin) const
     return 1.f;
 }
 
+// Called for every photon we send out from the light,
+// this function takes a reference to a 'Ray' object and
+// sets the position and direction of the ray
 void PointLight::GenerateRandomPhotonRay(Ray& ray) const
 {
-    // Assignment 7 TODO: Fill in the random point light samples here.
+    // set ray position to the point light position
+    ray.SetRayPosition(glm::vec3(this->GetPosition()));
+
+    // pick random number between -1 and 1 for x, y, and z
+    // by scaling rand() function, then normalize and use
+    // to set the ray direction
+        
+    // 0 to RAND_MAX
+    float x = 0.f;
+    float y = 0.f;
+    float z = 0.f;
+
+    do {
+
+        // 0 to RAND_MAX
+        x = static_cast<float>(rand());
+        y = static_cast<float>(rand());
+        z = static_cast<float>(rand());
+
+        // 0 to 1
+        x /= RAND_MAX;
+        y /= RAND_MAX;
+        z /= RAND_MAX;
+
+        // 0 to 2
+        x *= 2;
+        y *= 2;
+        z *= 2;
+
+        // -1 to 1
+        x -= 1;
+        y -= 1;
+        z -= 1;
+
+    } while ( (powf(x, 2) + powf(y, 2) + powf(z, 2)) > 1 );
+
+    glm::vec3 dir(x, y, z);
+
+    glm::vec3 ndir = glm::normalize(dir);
+
+    ray.SetRayDirection(ndir);
 }
